64fca95842a755786e78512d675c78b144bf2d8f
Pradyum Gupta <pradyumg@gmail.com>
2020-01-06 18:16:33 +0530

    update database file

--- inventory/settings.py
+++ inventory/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'InventoryMangment',
+        'NAME': 'inventorymangment',
         'USER': 'prady',
         'PASSWORD': 'harebol@123',
         'HOST': 'localhost',
