f30b0cc804445c47ad7342c24ed66121ac43440c
Pradyum Gupta <pradyumg@gmail.com>
2020-01-06 12:16:13 +0530

    update directory file

--- inventory/settings.py
+++ inventory/settings.py
@@ -12,10 +12,19 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-########################################################################
+
+##########################   STATIC  ######################################
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print('base dir: ',BASE_DIR)
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 print('project dir: ',PROJECT_DIR)
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
@@ -23,8 +32,6 @@ print('static dir: ',STATIC_ROOT)
 ########################################################################
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5@_^m^ufo^wjwf&dlb)cfb(qpepx7-n42fsggv+s=0zvp7io99'
@@ -125,11 +132,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.UserProfile'
 
